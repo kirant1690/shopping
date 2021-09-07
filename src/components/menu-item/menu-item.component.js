648361d564fb43af09fdfe3179b77f63bd3eb032
Kiran Thimmaiah <kiranthimmaiah@Kirans-MacBook-Air.local>
2021-09-07 15:54:06 +0530

    ui changes

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -5,8 +5,13 @@ const MenuItem = ({title, imageUrl, size}) => {
     return (
         <div style={{ backgroundImage: `url(${imageUrl})`}}
         className={`${size} menu-item`}>
+            <div className="background-image"
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            />
                     <div className="content">
-                        <h1 className="title">{title}</h1>
+                        <h1 className="title">{title.toUpperCase()}</h1>
                         <span className="subtitle">Shop Now</span>
                     </div>
                 </div>
